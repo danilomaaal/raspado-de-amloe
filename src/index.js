d73389b934df9a2c7ec88b0781496948594a2b6c
Daniel Mata <daniel.malugo@gmail.com>
2023-07-18 21:15:07 -0600

    my first scrapper
    attemps to scrap amlos interventions and so on and so on

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import puppeteer from "puppeteer"
+
+async function run (){
+// start session
+
+
+let browser = await puppeteer.launch({
+    headless: false, // visible browser to debug
+    defaultViewport :null, // website page in full width and height
+})
+
+    try { // open page
+        const page = await browser.newPage();
+        page.setDefaultNavigationTimeout(2 * 60 * 1000);
+
+        await page.goto("https://lopezobrador.org.mx/secciones/version-estenografica/", {
+            waitUntil: "domcontentloaded",
+          });   
+          
+        const entries = await page.evaluate( () => {
+            const internventions = document.querySelectorAll(".entry-post")
+            // generate an iterable array to get title and date for each entrie
+            return Array.from(internventions).map((entrie) => {
+                // get title and date
+                const title = entrie.querySelector(".entry-title").innerText;
+                const date = entrie.querySelector(".entry-date").innerText;
+                return { title, date };
+            });
+            
+        });
+  
+        console.log(entries);
+
+    } catch (err) {
+        console.error("Failed", err);
+    }
+    finally {
+        await browser?.close();
+    }
+}
+
+// start
+run();
