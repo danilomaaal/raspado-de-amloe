2f8f0afb1180072b4d7dee08e54a82129b88a97b
Daniel Mata <daniel.malugo@gmail.com>
2023-10-27 22:51:34 -0600

    include waiting for selectors, change loop order

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ async function next(page) {
 
 function waitRandomly() {
 	// select random number to wait
-	return Math.floor(Math.random() * (20 - 3) + Math.random() ) * 1000;
+	return Math.floor(Math.random() * (20 - 3 + 1) + 3) * 1000;
 }
 
 
@@ -22,12 +22,14 @@ async function run() {
 
 	// start session
 	let browser = await puppeteer.launch({
-		headless: true, // visible browser to debug
+		headless: false, // visible browser to debug
 		defaultViewport: null // website page in full width and height
 	})
 
     try { // open page
         const page = await browser.newPage();
+		const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36";
+		await page.setUserAgent(userAgent);
         page.setDefaultNavigationTimeout(2 * 60 * 1000);
 
         await page.goto("https://lopezobrador.org.mx/secciones/version-estenografica/", {
@@ -35,11 +37,14 @@ async function run() {
           });
 
 	    // iterate until 131th pag
-	   const pags = 131; 
-	   let data = [];
+	   const pags = 137; 
+	   let parsedData = [];
 
 	   for (let i = 1; i <= pags; i++){
 		   console.log("Now parsing page: ", i);
+
+		   await page.waitForSelector(".entry-post", { visible:true });
+
 		   const entries = await page.evaluate(() => {
 			   const interventions = document.querySelectorAll(".entry-post");
 			   // generate an iterable array to get title and date for each entry
@@ -56,35 +61,40 @@ async function run() {
 			   });
 		   });
 		   
-		   for (const entry of entries) {
-			console.log(`Now accessing: ${entry.link}`);
-	
-			await page.goto(entry.link, {
-				waitUntil: "domcontentloaded"
-			  });
-
-			entry.text = await page.evaluate(() => {
-				// @ts-ignore
-				return document.querySelector(".entry-content").innerText;
-			})
-			await sleep(waitRandomly()); // pause to avoid overloading server
-			await page.goBack();
-		   }
-
 		   console.log("Current object is: ", entries);
-		   
+		   await page.waitForSelector(".tw-pagination > .older > a",{ visible: true });
 		   next(page);
 
 		   await sleep(waitRandomly()); // pause again
 
-	       data.push(entries);
+	       parsedData.push(entries);
 		};
 
-	    // save obj to disk
-	    let JSONdata = JSON.stringify(data);
-		data = undefined;
+		for (const element of parsedData) {
+			for (const entry of element) {
+				console.log(`Now accessing: ${entry.link}`);
+				
+				await page.goto(entry.link, {
+					waitUntil: "domcontentloaded"
+				});
 
-	    fs.writeFile("data.json", JSONdata, "utf8", (err) => {
+				await page.waitForSelector(".entry-content", 
+				{ visible:true,
+					timeout: 60000
+				});
+				
+				entry.text = await page.evaluate(() => {
+					// @ts-ignore
+					return document.querySelector(".entry-content").innerText;
+				})
+				await sleep(waitRandomly()); // pause to avoid overloading server
+		}
+	}
+	    // save obj to disk
+	    let JSONdata = JSON.stringify(parsedData);
+		parsedData = undefined;
+
+	    fs.writeFile("text.json", JSONdata, "utf8", (err) => {
 			if (err) {
 				console.error("Error: ", err);
 			} else {
